43c2737bbda6f31a819282c9ce80da29e8893fca
Francesco Ilario <fr.ilario@gmail.com>
2020-05-02 02:41:42 +0000

    Introduced message sentiment analysis in bot

--- internal/bot/bot.go
+++ internal/bot/bot.go
@@ -2,28 +2,31 @@ package bot
 
 import (
 	"github.com/FrancescoIlario/why-so-serious-bot/pkg/wssface"
+	"github.com/FrancescoIlario/why-so-serious-bot/pkg/wsssentiment"
 	"github.com/FrancescoIlario/why-so-serious-bot/pkg/wssvision"
 	tb "gopkg.in/tucnak/telebot.v2"
 )
 
 //Bot WhySoSerious Bot implementation
 type Bot struct {
-	tbot      tb.Bot
-	faceCli   wssface.FaceServiceClient
-	visionCli wssvision.VisionServiceClient
+	tbot             tb.Bot
+	faceCli          wssface.FaceServiceClient
+	visionCli        wssvision.VisionServiceClient
+	textAnalyticsCli wsssentiment.TextAnalyticsServiceClient
 }
 
 //New Bot constructor
-func New(tbotSettings tb.Settings, faceConf wssface.Configuration, visionConf wssvision.Configuration) (*Bot, error) {
+func New(tbotSettings tb.Settings, faceConf wssface.Configuration, visionConf wssvision.Configuration, textAnalyticsConf wsssentiment.Configuration) (*Bot, error) {
 	tbot, err := tb.NewBot(tbotSettings)
 	if err != nil {
 		return nil, err
 	}
 
 	bot := &Bot{
-		tbot:      *tbot,
-		faceCli:   *wssface.NewFaceServiceClient(faceConf),
-		visionCli: *wssvision.NewVisionServiceClient(visionConf),
+		tbot:             *tbot,
+		faceCli:          *wssface.NewFaceServiceClient(faceConf),
+		visionCli:        *wssvision.NewVisionServiceClient(visionConf),
+		textAnalyticsCli: *wsssentiment.NewTextAnalyticsServiceClient(textAnalyticsConf),
 	}
 
 	tbot.Handle(tb.OnPhoto, bot.onPhoto)
