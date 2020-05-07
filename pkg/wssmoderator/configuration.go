ac5542498f57d6daa3b35396883458259541cfa1
Francesco Ilario <fr.ilario@gmail.com>
2020-05-07 14:46:47 +0000

    Added content moderator pkg

---
+++ pkg/wssmoderator/configuration.go
@@ -0,0 +1,53 @@
+package wssmoderator
+
+import (
+	"fmt"
+	"os"
+)
+
+//Configuration structure for the ContentModerator service configuration
+type Configuration struct {
+	ContentModeratorSubscription string
+	ServiceEnpoint               string
+}
+
+const (
+	//ContentModeratorSubscriptionKey Azure ContentModerator Subscription env key
+	ContentModeratorSubscriptionKey = "WSS_CONTENTMODERATOR_SUBSCRIPTION_KEY"
+	//ContentModeratorEndpointKey Azure ContentModerator Endpoint env key
+	ContentModeratorEndpointKey = "WSS_CONTENTMODERATOR_ENDPOINT"
+)
+
+//BuildConfigurationFromEnvs builds the configuration from env variables
+func BuildConfigurationFromEnvs() (*Configuration, error) {
+	sub, err := getContentModeratorSubscription()
+	if err != nil {
+		return nil, err
+	}
+
+	endStr, err := getContentModeratorEndpoint()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Configuration{
+		ContentModeratorSubscription: *sub,
+		ServiceEnpoint:               *endStr,
+	}, nil
+}
+
+func getContentModeratorSubscription() (*string, error) {
+	if v := os.Getenv(ContentModeratorSubscriptionKey); v != "" {
+		return &v, nil
+	}
+
+	return nil, fmt.Errorf("contentmoderator subscription env key (%s) not set", ContentModeratorSubscriptionKey)
+}
+
+func getContentModeratorEndpoint() (*string, error) {
+	if v := os.Getenv(ContentModeratorEndpointKey); v != "" {
+		return &v, nil
+	}
+
+	return nil, fmt.Errorf("contentmoderator subscription env key (%s) not set", ContentModeratorEndpointKey)
+}
