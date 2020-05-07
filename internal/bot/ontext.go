59b1dcf8ad7c35d5c1ff8dc481ac94661dc3af78
Francesco Ilario <fr.ilario@gmail.com>
2020-05-07 14:47:41 +0000

    Added content moderator in text handler

--- internal/bot/ontext.go
+++ internal/bot/ontext.go
@@ -2,26 +2,66 @@ package bot
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"strings"
 
+	"github.com/FrancescoIlario/why-so-serious-bot/pkg/wssmoderator"
+	"github.com/FrancescoIlario/why-so-serious-bot/pkg/wsssentiment"
 	tb "gopkg.in/tucnak/telebot.v2"
 )
 
-func (b *Bot) onText(m *tb.Message) {
-	log.Printf("received message: %v", m.Text)
-	textAnalyticsContext := context.Background()
-	res, err := b.textAnalyticsCli.InvokeTextAnalytics(textAnalyticsContext, m.Text)
-	if err != nil {
-		log.Printf("error invoking analytics service: %v", err)
+func (b *Bot) onText(m *tb.Message) (*tb.Message, error) {
+	var message string
+	ctx := context.Background()
+
+	mChan := b.invokeModeratorTextAPI(ctx, m.Text)
+	taChan := b.invokeTextAnalysisAPI(ctx, m.Text)
+
+	taRes := <-taChan
+	if score := taRes.SentimentScore; score != nil {
+		if *score <= 0.3 {
+			message += fmt.Sprintf("I feel you a little bit upset!")
+		} else if *score >= 0.7 {
+			message += fmt.Sprintf("What a nice positive message! Thanks!")
+		} else {
+			message += fmt.Sprintf("Why so serious?")
+		}
 	}
 
-	if score := res.SentimentScore; score == nil {
-		b.tbot.Send(m.Chat, "Sorry, I didn't understand what your sentiment is")
-	} else if *score <= 0.3 {
-		b.tbot.Send(m.Chat, "I feel you a little bit upset!")
-	} else if *score >= 0.7 {
-		b.tbot.Send(m.Chat, "What a nice positive message! Thanks!")
-	} else {
-		b.tbot.Send(m.Chat, "Why so serious?")
+	mRes := <-mChan
+	if len(mRes.BadWords) > 0 {
+		badlist := strings.Join(mRes.BadWords, ", ")
+		message += fmt.Sprintf("\nHowever, you should avoid to use bad words like: %s.", badlist)
 	}
+
+	return b.tbot.Send(m.Chat, message)
+}
+
+func (b *Bot) invokeModeratorTextAPI(ctx context.Context, text string) chan *wssmoderator.ContentModeratorTextResult {
+	mChan := make(chan *wssmoderator.ContentModeratorTextResult)
+	go func() {
+		defer close(mChan)
+		moderatorResult, err := b.moderatorCli.InvokeContentModeratorText(ctx, text)
+		if err != nil {
+			log.Printf("error invoking moderator service: %v", err)
+		}
+		mChan <- moderatorResult
+	}()
+
+	return mChan
+}
+
+func (b *Bot) invokeTextAnalysisAPI(ctx context.Context, text string) chan *wsssentiment.TextAnalyticsResult {
+	taChan := make(chan *wsssentiment.TextAnalyticsResult)
+	go func() {
+		defer close(taChan)
+		res, err := b.textAnalyticsCli.InvokeTextAnalytics(ctx, text)
+		if err != nil {
+			log.Printf("error invoking analytics service: %v", err)
+		}
+		taChan <- res
+	}()
+
+	return taChan
 }
