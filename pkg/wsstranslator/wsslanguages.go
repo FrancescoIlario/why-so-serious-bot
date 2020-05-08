3c27506417ae51e8b4547ea28e49896a579e73f5
Francesco Ilario <fr.ilario@gmail.com>
2020-05-08 00:01:48 +0000

    Added support for languages request

---
+++ pkg/wsstranslator/wsslanguages.go
@@ -0,0 +1,45 @@
+package wsstranslator
+
+import (
+	"context"
+)
+
+//InvokeLanguages invokes the Languages APIs to obtain the list of supported languages
+func (s *TranslatorServiceClient) InvokeLanguages(LanguagesContext context.Context) (*LanguagesResult, error) {
+	res, err := s.translatorCli.Languages(LanguagesContext, []string{`translation`}, `en`, "")
+	if err != nil {
+		return nil, err
+	}
+
+	translations := *res.Translation
+	if translations == nil {
+		return &LanguagesResult{}, nil
+	}
+
+	languages, counter := make([]Language, len(translations)), 0
+	for k, v := range *res.Translation {
+		var name string
+		if v.Name != nil {
+			name = *v.NativeName
+		} else if v.Name != nil {
+			name = *v.Name
+		} else {
+			name = ""
+		}
+
+		languages[counter] = Language{Code: k, Name: name}
+		counter++
+	}
+	return &LanguagesResult{Languages: languages}, nil
+}
+
+//LanguagesResult result of the LanguagesAPI
+type LanguagesResult struct {
+	Languages []Language
+}
+
+//Language describes a single language
+type Language struct {
+	Code string
+	Name string
+}
